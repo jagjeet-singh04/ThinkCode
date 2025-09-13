2957b19594a48568c2ba6685927527c0d4a77f27
Jagjeet <jagjeetsingh0424@gmail.com>
2025-09-13 17:42:29 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,4 @@
-// Global error handler for better debugging on Vercel
-app.use((err, req, res, next) => {
-  console.error('Express error:', err);
-  res.status(500).json({ error: 'Internal Server Error', details: err.message });
-});
+
 
 import express from 'express';
 import mongoose from 'mongoose';
@@ -19,7 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -27,12 +22,16 @@ mongoose.connect(MONGODB_URI, {
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-
 app.use('/api/auth', authRoutes);
-
 
 app.get('/', (req, res) => {
   res.send('API is running');
+});
+
+// Global error handler for better debugging on Vercel
+app.use((err, req, res, next) => {
+  console.error('Express error:', err);
+  res.status(500).json({ error: 'Internal Server Error', details: err.message });
 });
 
 export default app;
