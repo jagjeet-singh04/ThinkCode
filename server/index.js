0f7861bc501f928cb6fa346c1b47b3cac72a46f2
Jagjeet <jagjeetsingh0424@gmail.com>
2025-09-13 16:55:12 +0530

    index file updated

--- server/index.js
+++ server/index.js
@@ -14,16 +14,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch((err) => console.error('MongoDB connection error:', err));
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 app.use('/api/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
+export default app;
